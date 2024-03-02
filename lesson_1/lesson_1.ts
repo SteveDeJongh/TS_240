a918be6bfa788e5e07ca0303b0ea73e8b5acd5cc
Steve De Jongh <de.jongh.steve@gmail.com>
2024-03-02 15:19:20 -0800

    TS240 Lesson 1

---
+++ lesson_1/lesson_1.ts
@@ -0,0 +1,25 @@
+// Practice Problems 1
+
+// 1
+
+// Yes, because x and y are both numbers, resulting in addition rather than concatenation and returns a integer rather than a string.
+
+// 2
+
+// No, as this expression 2 + "2" will result in a string value type. 
+
+// 3
+
+// Yes, because the expression `2 === "2"` will raise an error stating that the comparison appears to be unintential as a string type and a number type will not have any overlap, and therefor the strict equality operator will never return true.
+
+// 4
+
+// No because of short circuit evaluation.
+
+// 5
+
+// Yes
+
+// 6
+
+// Yes, we can not reassign a `undefined` type varaible to a new type.
