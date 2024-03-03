4c695205460fc1eb4fb7c72e9214c685657c0c29
Steve De Jongh <de.jongh.steve@gmail.com>
2024-03-03 15:23:13 -0800

    TS240 Lesson 2

---
+++ lesson_2/lesson_2.ts
@@ -0,0 +1,14 @@
+// Lesson 2
+
+// Practice problems 1: Type Aliases
+
+// 1
+
+// Yes, this code will raise a typeerro because on lines 18 and 19 we are attempting to asign a number to a string type, and a string to a number type.
+
+// Practice Problems 2: Object Types
+
+// 1
+
+// Length
+
