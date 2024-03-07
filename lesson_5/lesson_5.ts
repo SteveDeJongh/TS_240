fc6dff0df247f7f84d5563a2213b2ffa55c75efa
Steve De Jongh <de.jongh.steve@gmail.com>
2024-03-07 15:22:59 -0800

    TS240 Lesson 5

--- lesson_5/lesson_5.ts
+++ lesson_5/lesson_5.ts
@@ -80,3 +80,60 @@ const item: ShippableProduct = {
   weight: 20,
   shippingCost: 30,
 };
+
+// Practice Problems: Differences between Interfaces and Types
+
+// 1
+
+// Yes, this will raise an error as types defined using the type aliases are `closed` and can not be changed after creation.
+
+type Point = { x: number };
+type Point = { y: number };
+
+const point: Point = { x: 1, y: 2 };
+
+// 2
+
+interface UserInterface {
+  name: string;
+  email: string;
+}
+
+type UserType = {
+  name: string;
+  email: string;
+};
+
+function greetUser(user: UserType) {
+  return `Hello, ${user.name}`;
+}
+
+const user: UserInterface = {
+  name: "Alice",
+  email: "alice@example.com",
+};
+
+console.log(greetUser(user));
+
+// No this will not result in an error. As even though we are passing in a `user` type to `greetUser` instead of a `userType` type, thanks to TypeScripts structural type system, were concerned with the shape that the values rather than the name of the type.
+
+// Practice Problems: Index Signatures
+
+// 1
+
+interface User {
+  [key: number]: string;
+}
+
+const obj: User = {
+  1: "Jane",
+  2: "30",
+  3: "female",
+};
+
+console.log(Object.keys(obj).every((key) => typeof key === "number"));
+
+// This will result in false due to how JavaScript automatically converts keys to strings.
+
+// 2
+
